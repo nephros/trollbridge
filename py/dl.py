6831076e8e0be2743c7f73d0a30668939813a2a7
nephros (PineTab) <sailfish@nephros.org>
2023-03-26 22:43:53 +0200

    begin moving to python from workerscript

--- py/dl.py
+++ py/dl.py
@@ -2,10 +2,26 @@
 from binascii import a2b_base64
 #import base64
 import pyotherside
-import pycurl
+import pycurl # we use this instead of Requests because it is pre-installed on SFOS
+import StringIO
+
 
 def downloadList (model, mode):
+    print "NOT IMPLEMENTED."
 
+def getAndWriteImage (url , path):
+    hbuf = StringIO.StringIO()
+    with open(path, 'wb') as f:
+        c = pycurl.Curl()
+        c.setopt(c.URL, url)
+        c.setopt(c.WRITEDATA,f)
+        c.setopt(c.HTTPHEADER, ['User-Agent:', 'OlympusCameraKit' ])
+        c.setopt(c.HTTPHEADER, ['Host:', '192.168.0.10' ])
+        c.setopt(c.HEADERFUNCTION, hbuf.write)
+        c.perform
+        c.close
+        header = hbuf.getvalue()
+        print header
 
 def writeImage (data , path):
     #rawdata = base64.b64decode(data)
