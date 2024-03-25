7479656de70276bcd002b785375acc11a83ce393
nephros <nemo@pgxperiiia10>
2024-03-25 15:03:45 +0100

    Add linting to build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 specify -Nns rpm/*yaml || exit 1
+printf linting...
+qmllint qml/*.qml
+python3 -m py_compile py/*.py
 printf building...
 rpmbuild -bb --build-in-place rpm/*.spec > build.log 2>&1
 printf "exit: $?\n"
